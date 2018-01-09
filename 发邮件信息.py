87818ca3ec7c178e0640a333717846dffa273641
HaiDong <1045866989@qq.com>
2018-01-09 15:06:19 +0800

    xxx

--- 发邮件信息.py
+++ 发邮件信息.py
@@ -4,10 +4,10 @@ from email.utils import formataddr
 
 msg = MIMEText('傻子吧！', 'plain', 'utf-8')  # 发送内容
 msg['From'] = formataddr(["haidong", 'nhd104586@126.com'])  # 发件人
-msg['To'] = formataddr(["ali", '2981405421@qq.com'])  # 收件人
+msg['To'] = formataddr(["xxxx", 'xxxxxxx@qq.com'])  # 收件人
 msg['Subject'] = "【请回复】测试"  # 主题
 
 server = smtplib.SMTP("smtp.126.com", 25) # SMTP服务
-server.login("nhd104586@126.com", "daohaogou213") # 邮箱用户名和密码
+server.login("xxxxxxxx@126.com", "xxxxxxxx") # 邮箱用户名和密码
 server.sendmail('nhd104586@126.com', ['2981405421@qq.com', ], msg.as_string()) # 发送者和接收者
 server.quit()
